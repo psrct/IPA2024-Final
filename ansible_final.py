34566b53dea568723923de87fc6a85a51ec161a2
psrct <sirachet.c@gmail.com>
2025-10-23 01:34:18 +0700

    complete command showrun by ansible add ansible playbook/cfg/hosts.yaml

--- ansible_final.py
+++ ansible_final.py
@@ -1,11 +1,20 @@
 import subprocess
 
+STUDENT_ID = "66070191"
+
 def showrun():
-    # read https://www.datacamp.com/tutorial/python-subprocess to learn more about subprocess
-    command = ['<!!!REPLACEME with ansible command to run playbook!!!>', '<!!!REPLACEME with playbook yaml file!!!>']
-    result = subprocess.run(command, capture_output=True, text=True)
-    result = result.stdout
-    if 'ok=2' in result:
-        return <!!!REPLACEME!!!>
-    else:
-        return '<!!!REPLACEME!!!>
+    command = ['ansible-playbook', '-i', 'hosts.yaml', '-e', f'STUDENT_ID={STUDENT_ID}', 'newplaybook.yaml']
+    try:
+        result = subprocess.run(command, capture_output=True, text=True, check=True)
+        if 'failed=0' in result.stdout:
+            print("Ansible successfully.")
+            return "ok"
+        else:
+            print("Ansible failed.")
+            print(result.stderr)
+            return "Error: Ansible"
+            
+    except subprocess.CalledProcessError as e:
+        print("Error running ansible-playbook command.")
+        print(e.stderr)
+        return "Error: Ansible"
